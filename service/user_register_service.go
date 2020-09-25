262f7bbbef938ec40ceafbf5bcef8b7516366b17
117503445 <t117503445@gmail.com>
2020-09-25 09:45:08 +0800

    refactor: remove PasswordConfirm

--- service/user_register_service.go
+++ service/user_register_service.go
@@ -10,17 +10,10 @@ type UserRegisterService struct {
 	Nickname        string `form:"nickname" json:"nickname" binding:"required,min=2,max=30"`
 	UserName        string `form:"user_name" json:"user_name" binding:"required,min=5,max=30"`
 	Password        string `form:"password" json:"password" binding:"required,min=8,max=40"`
-	PasswordConfirm string `form:"password_confirm" json:"password_confirm" binding:"required,min=8,max=40"`
 }
 
 // valid 验证表单
 func (service *UserRegisterService) valid() *serializer.Response {
-	if service.PasswordConfirm != service.Password {
-		return &serializer.Response{
-			Code: 40001,
-			Msg:  "两次输入的密码不相同",
-		}
-	}
 
 	count := 0
 	model.DB.Model(&model.User{}).Where("nickname = ?", service.Nickname).Count(&count)
